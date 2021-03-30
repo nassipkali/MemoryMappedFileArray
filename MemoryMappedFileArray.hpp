1d32469aee759ffdd3ab52dd316295c2234a0cc7
Daulet <nassipkali>
2021-03-30 22:02:45 +0500

    Removed Windows support

--- MemoryMappedFileArray.hpp
+++ MemoryMappedFileArray.hpp
@@ -2,22 +2,15 @@
 #define MEMORYMAPPEDFILEARRAY_HPP
 #include <stddef.h>
 #include <stdexcept>
-#if defined (unix)
-typedef int fd_t;
-#endif
-
-#if defined (WIN32)
-typedef struct fd {
-  void* file_handle;
-  void* mapping_handle;
-} fd_t;
-#endif
-
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
 
 template <typename T>
 class MemoryMappedFileArray {
     public:
-	fd_t file;
+	int file;
 	size_t block_size;
 	T* mapped_array;
 	size_t size = 0;
@@ -29,14 +22,6 @@ class MemoryMappedFileArray {
 	void Add(T element);
 	T& operator[] (size_t index);
 };
-
-
-// UNIX
-#if defined (unix)
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
 
 template <typename T>
 MemoryMappedFileArray<T>::MemoryMappedFileArray(const char* filename, size_t block_size) {
@@ -71,45 +56,6 @@ void MemoryMappedFileArray<T>::Realloc(size_t cap) {
         throw std::runtime_error("MemoryMappedFileArray::Realloc(size_t cap) Error: mapped_array is NULL");
     }
 }
-#endif
-
-//WINDOWS
-#if defined (WIN32)
-template <typename T>
-MemoryMappedFileArray<T>::MemoryMappedFileArray(const char* filename, size_t block_size = 4096) {
-    this->file.file_handle = CreateFileA(filename, GENERIC_READ | GENERIC_WRITE, 0, nullptr, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
-    DWORD file_size = GetFileSize(this->file.file_handle, nullptr);
-    this->size = file_size;
-    this->cap = (this->size + sizeof(T) / block_size + 1) * block_size;
-    FileResize(this->cap);
-    this->file.mapping_handle = CreateFileMapping(this->file.file_handle, nullptr, PAGE_READWRITE, 0, (DWORD)this->cap, nullptr);
-    this->mapped_array = (T*)MapViewOfFile(this->file.mapping_handle, FILE_MAP_READ | FILE_MAP_WRITE, 0, 0, (DWORD)this->cap);
-    if(!this->mapped_array) {
-	throw std::runtime_error("MemoryMappedFileArray::MemoryMappedFileArray(const char* filename, size_t block_size) Error: mapped_array is NULL");	
-    }
-}
-
-template <typename T>
-MemoryMappedFileArray<T>::~MemoryMappedFileArray() {
-    UnMapViewOfFile(this->mapped_array);
-    SetFilePointer(this->file.file_handle, this->size, nullptr, FILE_BEGIN);
-    SetEndOfFile(this->file.file_handle);
-}
-
-template <typename T>
-void MemoryMappedFileArray<T>::Realloc(size_t cap) {
-    this->cap = cap;
-    UnMapViewOfFile(this->mapped_array);
-    CloseHandle(this->file.mapping_handle);
-    SetFilePointer(this->file.file_handle, this->cap, nullptr, FILE_BEGIN);
-    SetEndOfFile(this->file.file_handle);
-    this->file.mapping_handle = CreateFileMapping(this->file.file_handle, nullptr, PAGE_READWRITE, 0, (DWORD)this->cap, nullptr);
-    this->mapped_array = (T*)MapViewOfFile(this->file.mapping_handle, FILE_MAP_READ | FILE_MAP_WRITE, 0, 0, (DWORD)this->cap);
-    if(!this->mapped_array) {
-	throw std::runtime_error("MemoryMappedFileArray::Realloc(size_t cap) Error: mapped_array is NULL");
-    }
-}
-#endif
 
 template <typename T>
 void MemoryMappedFileArray<T>::Add(T element) {
